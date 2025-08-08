960f13f10c2adb5d18ea286b7d8f78068a4a7a89
Abdellah-Nsila <dawndawn074@gmail.com>
2025-08-08 19:08:02 +0100

    Setup Account.cpp

---
+++ CPP00/ex02/Account.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 19:00:31 by abnsila           #+#    #+#             */
+/*   Updated: 2025/08/08 19:07:22 by abnsila          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "Account.hpp"
+
+// ------------------------------------ First Methods -------------------------------
+int Account::getNbAccounts()
+{
+	
+}
+
+int Account::getTotalAmount()
+{
+	
+}
+
+int Account::getNbDeposits()
+{
+	
+}
+
+int Account::getNbWithdrawals()
+{
+	
+}
+
+void Account::displayAccountsInfos()
+{
+	
+}
+
+// ------------------------------------ Second Methods -------------------------------
+void	Account::makeDeposit( int deposit )
+{
+	
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	
+}
+
+int	Account::checkAmount() const
+{
+	
+}
+
+void	Account::displayStatus() const
+{
+	
+}
