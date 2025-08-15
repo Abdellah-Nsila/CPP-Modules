fc33487c92b543802676fb166cee8340b10db8e2
Abdellah-Nsila <dawndawn074@gmail.com>
2025-08-15 16:31:34 +0100

    Add Source Redme file into Cpp02

---
+++ CPP02/Readme.md
@@ -0,0 +1,12 @@
+# Sources:
+
+[Polymorphism in C++, We focus on Ad-hoc polymorphism type](https://fluentprogrammer.com/polymorphism-in-cpp/#ad-hoc-polymorphism-overloading)
+
+[Operator Overloading in C++](https://www.geeksforgeeks.org/cpp/operator-overloading-cpp/)
+
+[Understanding the Orthodox Canonical Class Form](https://riceset.com/C++/The-Orthodox-Canonical-Class-Form)
+
+[C++ Fixed-Point Number](https://hackmd.io/@ChloeIsCoding/Bk8gPG1Mxx)
+
+[Cpp02/ex03: Binary Space Partitioning (BSP) for Point-in-triangle test ](https://hackmd.io/@ChloeIsCoding/r19Mge-fll#)
+
--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 12:18:29 by abnsila           #+#    #+#             */
-/*   Updated: 2025/08/15 15:58:45 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/08/15 16:06:39 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,18 @@ string	boolToString(bool flag)
 
 int main( void )
 {
+	bool isInside;
 	Point a(0.0f, 0.0f);
 	Point b(1.0f, 7.0f);
 	Point c(10.0f, 1.0f);
 
-	Point point(2.75f, 5.5f);
+	Point point1(2.75f, 5.5f);
+	Point point2(10.0f, 2.5f);
 	
-	bool isInside = bsp(a, b, c, point);
+	isInside = bsp(a, b, c, point1);
+	cout << boolToString(isInside) << endl;
 
+	isInside = bsp(a, b, c, point2);
 	cout << boolToString(isInside) << endl;
 
 	return 0;
