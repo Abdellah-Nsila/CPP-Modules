7c0f8314bc100845f709baf1166358ebe1bdfff2
Abdellah-Nsila <dawndawn074@gmail.com>
2025-09-22 11:40:31 +0100

    Update CPP03

--- CPP03/ex03/main.cpp
+++ CPP03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 16:37:07 by abnsila           #+#    #+#             */
-/*   Updated: 2025/09/22 10:56:42 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/09/22 11:38:52 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,46 @@
 
 int main(void)
 {
+
+	// ============================== ClapTrap Test ==============================
+    ClapTrap bot1("Alpha");
+    ClapTrap bot2("Bravo");
+    ClapTrap bot3(bot2);
+
+    bot1.attack("Bravo");
+    bot3.takeDamage(5);
+    bot3.beRepaired(3);
+    bot3.attack("Alpha");
+    for (int i = 0; i < 10; i++)
+        bot1.attack("Bravo");
+    bot1.attack("Bravo");
+    bot1.beRepaired(5);
+
+
+    // ============================== ScavTrap Test ==============================
+    ScavTrap scav("Guardian");
+
+    scav.attack("intruder");
+    scav.takeDamage(30);
+    scav.beRepaired(20);
+    scav.guardGate();
+
+    std::cout << "----- Copy & assignment tests -----" << std::endl;
+    ScavTrap scav2(scav);
+    ScavTrap scav3("temp");
+    scav3 = scav;
+
+	// ============================== FragTrap Test ==============================
+	FragTrap F1("Beta");
+	F1.attack("Enemy1");
+	F1.highFivesGuys();
+    FragTrap F2("Charlie");
+	FragTrap F3(F2);
+    F3.takeDamage(100);
+    F3.takeDamage(10);
+	
+	// ============================== DiamondTrap Test ==============================
+
 	DiamondTrap A;
 	DiamondTrap B("B");
 	DiamondTrap C = B;
