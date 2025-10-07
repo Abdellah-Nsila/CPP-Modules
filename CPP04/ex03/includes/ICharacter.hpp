945519de437ef23a1c358427bee046b09c03ab29
Abdellah-Nsila <dawndawn074@gmail.com>
2025-10-07 19:07:08 +0100

    Build ex structure

--- CPP04/ex03/includes/ICharacter.hpp
+++ CPP04/ex03/includes/ICharacter.hpp
@@ -6,7 +6,7 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 16:36:31 by abnsila           #+#    #+#             */
-/*   Updated: 2025/10/06 17:33:19 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/10/07 15:40:17 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ class ICharacter
 {
 	public:
 		virtual ~ICharacter() {}
-		virtual std::string const & getName() const = 0;
+		virtual std::string const& getName() const = 0;
 		virtual void equip(AMateria* m) = 0;
 		virtual void unequip(int idx) = 0;
 		virtual void use(int idx, ICharacter& target) = 0;
