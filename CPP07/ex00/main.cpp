7031827720d1b15bb22861dc4e058b82abb5d0c9
Abdellah-Nsila <dawndawn074@gmail.com>
2025-11-19 17:02:17 +0100

    Updates

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 15:52:06 by abnsila           #+#    #+#             */
-/*   Updated: 2025/11/19 16:51:21 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/11/19 16:54:30 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,59 @@
 #include <iostream>
 #include <string>
 
-int	main()
+int main( void )
 {
-	int 		i1, i2;	
-	double		d1, d2;
-	std::string	s1, s2;
-
-	i1 = 5;
-	i2 = 10;
-
-	d1 = 7.5;
-	d2 = 2.25;
-
-	s1 = "Hello";
-	s2 = "World";
-
-	std::cout << "=========================== Before ===========================" << std::endl;
-	std::cout << "int 1: " << i1 << ", int 2: " << i2 << std::endl;
-	std::cout << "double 1: " << d1 << ", double 2: " << d2 << std::endl;
-	std::cout << "string 1: " << s1 << ", string 2: " << s2 << std::endl;
-	
-	swap(i1, i2);
-	swap(d1, d2);
-	swap(s1, s2);
-	
-	std::cout << "=========================== Swap ===========================" << std::endl;
-	std::cout << "int 1: " << i1 << ", int 2: " << i2 << std::endl;
-	std::cout << "double 1: " << d1 << ", double 2: " << d2 << std::endl;
-	std::cout << "string 1: " << s1 << ", string 2: " << s2 << std::endl;
-	
-	std::cout << "=========================== Min ===========================" << std::endl;
-	std::cout << "Min: " << min(i1, i2) << std::endl;
-	std::cout << "Min: " << min(d1, d2) << std::endl;
-	std::cout << "Min: " << ::min(s1, s2) << std::endl;
-	
-	std::cout << "=========================== Max ===========================" << std::endl;
-	std::cout << "Max: " << max(i1, i2) << std::endl;
-	std::cout << "Max: " << max(d1, d2) << std::endl;
-	std::cout << "Max: " << ::max(s1, s2) << std::endl;
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
+
+// int	main()
+// {
+// 	int 		i1, i2;	
+// 	double		d1, d2;
+// 	std::string	s1, s2;
+
+// 	i1 = 5;
+// 	i2 = 10;
+
+// 	d1 = 7.5;
+// 	d2 = 2.25;
+
+// 	s1 = "Hello";
+// 	s2 = "World";
+
+// 	std::cout << "=========================== Before ===========================" << std::endl;
+// 	std::cout << "int 1: " << i1 << ", int 2: " << i2 << std::endl;
+// 	std::cout << "double 1: " << d1 << ", double 2: " << d2 << std::endl;
+// 	std::cout << "string 1: " << s1 << ", string 2: " << s2 << std::endl;
+	
+// 	swap(i1, i2);
+// 	swap(d1, d2);
+// 	swap(s1, s2);
+	
+// 	std::cout << "=========================== Swap ===========================" << std::endl;
+// 	std::cout << "int 1: " << i1 << ", int 2: " << i2 << std::endl;
+// 	std::cout << "double 1: " << d1 << ", double 2: " << d2 << std::endl;
+// 	std::cout << "string 1: " << s1 << ", string 2: " << s2 << std::endl;
+	
+// 	std::cout << "=========================== Min ===========================" << std::endl;
+// 	std::cout << "Min: " << min(i1, i2) << std::endl;
+// 	std::cout << "Min: " << min(d1, d2) << std::endl;
+// 	std::cout << "Min: " << ::min(s1, s2) << std::endl;
+	
+// 	std::cout << "=========================== Max ===========================" << std::endl;
+// 	std::cout << "Max: " << max(i1, i2) << std::endl;
+// 	std::cout << "Max: " << max(d1, d2) << std::endl;
+// 	std::cout << "Max: " << ::max(s1, s2) << std::endl;
+// }
