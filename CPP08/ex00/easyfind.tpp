0837e8e5ef9d897503b048c2ac984f129e6f906c
Abdellah-Nsila <dawndawn074@gmail.com>
2025-12-22 09:20:58 +0100

    Understand Iterators

--- CPP08/ex00/easyfind.tpp
+++ CPP08/ex00/easyfind.tpp
@@ -7,6 +7,8 @@ void	easyfind(T& container, int toFind)
 	(void)toFind;
 	for(typename T::iterator it = container.begin(); it != container.end(); it++)
 	{
+		// if (*it == toFind)
+		// 	return ()		
 		std::cout << *it << std::endl;
 	}
 }
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include "easyfind.hpp"
 
+#include <algorithm>
+
 int	main()
 {
-	std::vector<int> container;
+	typedef std::vector<int>		IntContainer;
+	typedef IntContainer::iterator	IntIterator;
+
+	IntContainer	container;
+	IntIterator		it;
 	
-	container.push_back(1);
+	container.push_back(0);
 	container.push_back(2);
 	container.push_back(3);
+
+
+	it = std::find(container.begin(), container.end(), 2);
+	if (it != container.end())
+		std::cout << "Found" << std::endl;
+	else
+		std::cout << "Not Found" << std::endl;
 
 	easyfind(container, 2);
 	return 0;
