c6778a391b1a3ff9093426cc49907dd1afc13e18
Abdellah-Nsila <dawndawn074@gmail.com>
2026-02-07 12:33:16 +0100

    Begin CPP08

---
+++ CPP08/ex02/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/07 11:22:25 by abnsila           #+#    #+#             */
+/*   Updated: 2026/02/07 11:23:01 by abnsila          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main()
+{
+	MutantStack<int> mstack;
+	
+	mstack.push(5);
+	mstack.push(17);
+	
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	
+	std::stack<int> s(mstack);
+	
+	return 0;
+}
