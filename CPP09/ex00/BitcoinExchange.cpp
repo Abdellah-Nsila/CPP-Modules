7341012083676cc667580bb2472abd1f12ca40c8
Abdellah-Nsila <dawndawn074@gmail.com>
2026-02-13 22:55:46 +0100

    Begin CPP09

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,21 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 18:58:05 by abnsila           #+#    #+#             */
-/*   Updated: 2026/02/12 18:58:06 by abnsila          ###   ########.fr       */
+/*   Updated: 2026/02/13 22:41:14 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <fstream>
+
+int	parseBtcHistory(std::string f)
+{
+	std::ifstream	file(f.c_str());
+	if (!file.is_open())
+	{
+		std::cerr << "Error: could not open file." << std::endl;
+		return 1;
+	}
+	
+	return 0;
+}
